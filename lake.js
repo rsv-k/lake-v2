69a1facd827bf528899e865a8adedbf89091d2a4
unknown <rsv.kpast@gmail.com>
2020-11-29 23:01:22 +0200

    comment message event

--- lake.js
+++ lake.js
@@ -8,15 +8,15 @@ if (process.env.NODE_ENV !== 'production') {
 
 lake.on('ready', () => console.log(`Logged in as ${lake.user.tag}!`));
 
-lake.on('message', (msg) => {
-   if (msg.author.id !== '481189853241802792') {
-      return;
-   }
+// lake.on('message', (msg) => {
+//    if (msg.author.id !== '481189853241802792') {
+//       return;
+//    }
 
-   const [command, id] = msg.content.split(' ');
-   if (command === `${config.perfix}mute`) {
-      msg.reply('pong');
-   }
-});
+//    const [command, id] = msg.content.split(' ');
+//    if (command === `${config.perfix}mute`) {
+//       msg.reply('pong');
+//    }
+// });
 
 lake.login(process.env.TOKEN);
