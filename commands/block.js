ccde131c90ccb4f1d64bcddf316970649bb41b74
unknown <rsv.kpast@gmail.com>
2020-11-30 17:03:53 +0200

    add block command

--- commands/block.js
+++ commands/block.js
@@ -1,12 +1,30 @@
 const fs = require('fs');
 
-module.exports = (id) => {
-   fs.readFile('../config.js', (err, data) => {
+module.exports = (msg, ids) => {
+   const id = ids[0];
+   if (!id || id.length !== '481189853241802792'.length) {
+      return msg.reply('Invalid id');
+   }
+
+   fs.readFile('config.json', (err, data) => {
       if (err) {
          throw err;
       }
 
-      const config = JSON.parse(data);
-      console.log(config);
+      let config = JSON.parse(data);
+      if (config.forbiddenIds.includes(id)) {
+         return msg.reply('Such id has already been blocked');
+      }
+
+      config.forbiddenIds.push(id);
+
+      config = JSON.stringify(config, null, 3);
+      fs.writeFile('config.json', config, (err) => {
+         if (err) {
+            throw err;
+         }
+
+         msg.reply('Id has been blocked');
+      });
    });
 };
--- events/message.js
+++ events/message.js
@@ -1,1 +1,19 @@
-module.exports = (msg) => {};
+const commandsHandler = require('../commandsHandler');
+const config = require('../config.json');
+
+module.exports = (msg) => {
+   if (msg.author.id !== config.ownerId) {
+      return;
+   }
+
+   const [command, ...args] = msg.content.split(' ');
+   if (!command.startsWith(config.perfix)) {
+      return;
+   }
+
+   try {
+      commandsHandler[command.slice(2)](msg, args);
+   } catch (err) {
+      console.log(err.message);
+   }
+};
